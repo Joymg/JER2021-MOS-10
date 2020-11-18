dc283b0e6c68ef310088e4710c1c977419635964
Joymg <josemg98and@gmail.com>
2020-11-18 12:59:07 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,7 +10,16 @@ var config = {
         //ddebug :true
       },
     },
+<<<<<<< Updated upstream
     scene: [GameplayScene,MainMenu,CharacterSelection,SettingsMenu,PauseMenu]
+=======
+<<<<<<< Updated upstream
+    scene: [MainMenu,CharacterSelection,SettingsMenu,PauseMenu]
+=======
+    //el qeu va primero es el qeu se ve al iniciar, bueno para testeat
+    scene: [GameplayScene,CharacterSelection,MainMenu,SettingsMenu,PauseMenu]
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
   };
 
   var game = new Phaser.Game(config);
