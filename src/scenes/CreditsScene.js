65cf2053015c95bc46e7f28a9fdaed5dde3f9871
Joymg <josemg98and@gmail.com>
2020-12-15 13:26:06 +0100

    Mejora Botones

--- src/scenes/CreditsScene.js
+++ src/scenes/CreditsScene.js
@@ -7,7 +7,7 @@ class CreditsScene extends Phaser.Scene {
   create() {
     var background = this.add
       .image(this.game.renderer.width / 2, this.game.renderer.height / 2, "sky")
-      .setScale(1.3);
+      .setScale(1.5);
 
     var title = this.add
       .text(this.game.renderer.width / 2, (this.game.renderer.height * 0.5) / 5, "Credits", {
@@ -15,36 +15,51 @@ class CreditsScene extends Phaser.Scene {
       })
       .setOrigin(0.5);
 
-    var schair = this.add.text(
-      50,
-      (game.renderer.height * 1.5) / 5,
-      "Schair Álvarez \n    Maniega",
-      {
+    var schair = this.add
+      .text((this.game.renderer.width * 1) /6, (game.renderer.height * 1.5) / 5, "Schair Álvarez \n    Maniega", {
         font: "20px",
-      }
-    );
-    var andrea = this.add.text(
-      330,
-      (game.renderer.height * 1.5) / 5,
-      "Andrea María \nHodas Tortosa",
-      {
-        font: "20px",
-      }
-    );
-    var joy = this.add.text(580, (game.renderer.height * 1.5) / 5, "Jose Márquez\n   García", {
-      font: "20px",
-    });
-    var alberto = this.add.text(
-      150,
-      (game.renderer.height * 3) / 5,
-      "Alberto Sánchez \n    Romero",
-      {
-        font: "20px",
-      }
-    );
-    var marta = this.add.text(500, (game.renderer.height * 3) / 5, "Marta Vidal \n  Gonzalez", {
-      font: "20px",
-    });
+      })
+      .setOrigin(0.5);
+    var andrea = this.add
+      .text(
+        (this.game.renderer.width * 3) / 6,
+        (game.renderer.height * 1.5) / 5,
+        "Andrea María \nHodas Tortosa",
+        {
+          font: "20px",
+        }
+      )
+      .setOrigin(0.5);
+    var joy = this.add
+      .text(
+        (this.game.renderer.width * 5) / 6,
+        (game.renderer.height * 1.5) / 5,
+        "Jose Márquez\n   García",
+        {
+          font: "20px",
+        }
+      )
+      .setOrigin(0.5);
+    var alberto = this.add
+      .text(
+        (this.game.renderer.width * 2) / 6,
+        (game.renderer.height * 3.25) / 5,
+        "Alberto Sánchez \n    Romero",
+        {
+          font: "20px",
+        }
+      )
+      .setOrigin(0.5);
+    var marta = this.add
+      .text(
+        (this.game.renderer.width * 4) / 6,
+        (game.renderer.height * 3.25) / 5,
+        "Marta Vidal \n  Gonzalez",
+        {
+          font: "20px",
+        }
+      )
+      .setOrigin(0.5);
 
     //Boton de atras
     let backButton = this.add.image(
@@ -56,7 +71,7 @@ class CreditsScene extends Phaser.Scene {
 
     backButton.setInteractive();
     backButton.on("pointerover", () => {
-      backButton.setTint(0x202020);
+      backButton.setTint(0x909090);
     });
     backButton.on("pointerout", () => {
       backButton.setTint();
