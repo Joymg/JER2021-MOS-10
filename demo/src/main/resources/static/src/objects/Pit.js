e9777bb89ea1de1ace54b4d18d0fc758edac0d36
Joymg <josemg98and@gmail.com>
2021-01-16 17:49:35 +0100

    (??)

--- demo/src/main/resources/static/src/objects/Pit.js
+++
@@ -1,9 +0,0 @@
-class Pit extends Obstacle{
-    constructor(scene,xPos,yPos){
-        super(scene,xPos,yPos,"pitSprite")
-        this.isDestructible = false;
-        this.bulletsGoThrough = true;
-        this.setScale(0.9);
-    }
-
-}
