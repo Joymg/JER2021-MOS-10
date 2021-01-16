05559e10d78327c398392facfdeb5a4319e4d389
Andrea Mª Hodas Tortosa <60574895+AndreaHodas@users.noreply.github.com>
2021-01-16 21:45:03 +0100

    Prototipo login (en proceso)

---
+++ demo/src/main/resources/static/src/form.js
@@ -0,0 +1,5 @@
+function formulario(){
+    document.getElementById("login").style.display = "none";
+    document.getElementById("centro").style.display = "block";
+    document.getElementById("controles").style.visibility = "visible";
+}
