dc3ff7e902dd58592291aaba77ae445915bca143
Schair <schairamaniega@gmail.com>
2021-01-18 21:11:26 +0100

    chat amochao (que funciona regular, vaya)

--- demo/src/main/resources/static/src/form.js
+++ demo/src/main/resources/static/src/form.js
@@ -18,8 +18,6 @@ function postPetition() {
             Accept: "application/json",
             "Content-Type": "application/json",
         },
-
-        //make sure to serialize your JSON body
         body: JSON.stringify({
         nickName: $("#nickName").val(),
         }),
@@ -30,9 +28,34 @@ function postPetition() {
         document.getElementById("login").style.display = "none";
         document.getElementById("centro").style.display = "block";
         document.getElementById("controles").style.visibility = "visible";
+        document.getElementById("chatBox").style.display = "block";
     } else if(!failed){
         $("#login").append("<br><p id='mess'>Debes introducir un nombre para poder jugar</p>");
         failed = true;
     }
 }
-    
+function postMessage(){
+    var writtenMessage = $("#message").val();
+    if(writtenMessage != null){
+        fetch("http://localhost:8080/chat", {
+            method: "POST",
+            headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+        name: displayName,
+        message: $("#message").val(),
+        }),
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+        $("#chatHistory").append("<p><b>"+ displayName + ": </b>" + writtenMessage + "</p>");
+    }
+}
+function getMessage(){
+    fetch("http://localhost:8080/chat")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+}
+
